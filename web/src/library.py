4018538ef90aa74087ded9f7fd9a87df5655168c
yooj425 <yooj425@gmail.com>
2026-06-01 08:59:36 +0900

    python file and docker MYSQL

---
+++ web/src/library.py
@@ -0,0 +1,20 @@
+import mysql.connector
+
+conn = mysql.connector.connect(
+    host="127.0.0.1",
+    user="root",
+    password="1234",
+    database="library",
+    port=3308
+)
+
+cursor = conn.cursor()
+
+cursor.execute("SELECT * FROM members")
+
+result = cursor.fetchall()
+
+for row in result:
+    print(row)
+
+conn.close()
