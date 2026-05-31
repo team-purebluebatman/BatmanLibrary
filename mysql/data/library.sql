499ed67bbfd6b87b5ef1d1b3806fbeb1b7e486f2
yooj425 <yooj425@gmail.com>
2026-06-01 08:34:39 +0900

    sql 추가

---
+++ mysql/data/library.sql
@@ -0,0 +1,23 @@
+create database library;
+use library;
+create table books(
+    book_id int primary key auto_increment,
+    book_name varchar(255) not null,
+    author varchar(100) not null,
+    publisher varchar(100) not null
+)
+create table members(
+    member_id int primary key auto_increment,
+    member_name varchar(255) not null,
+    email varchar(100) not null unique,
+    phone varchar(20) not null
+)
+create table loans(
+    loan_id int primary key auto_increment,
+    book_id int,
+    member_id int,
+    loan_date date,
+    return_date date,
+    foreign key (book_id) references books(book_id),
+    foreign key (member_id) references members(member_id)
+)
